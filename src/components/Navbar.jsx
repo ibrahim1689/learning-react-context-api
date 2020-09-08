c8695d63d406ea9eed5456dc6089b207a1ea934c
ibrahim <hello.dev.ibrahim@gmail.com>
2020-09-08 16:19:05 +0530

    Used ThemeContext.Consumer in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,39 +4,41 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends Component {
   // state = {  }
 
-  static contextType = ThemeContext;
-
   render() {
-    console.log(this.context);
-    const { isLightTheme, dark, light } = this.context;
-
-    const theme = isLightTheme ? light : dark;
-
     return (
-      <nav
-        style={{
-          color: theme.text,
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, dark, light } = context;
+
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav
+              style={{
+                color: theme.text,
+              }}
+            >
+              <h1
+                style={{
+                  background: theme.bg,
+                  boxShadow: `0px 0px 20px ${theme.ui}`,
+                }}
+              >
+                Context App
+              </h1>
+              <ul
+                style={{
+                  boxShadow: `0px 0px 20px ${theme.ui}`,
+                  background: theme.bg,
+                }}
+              >
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          );
         }}
-      >
-        <h1
-          style={{
-            background: theme.bg,
-            boxShadow: `0px 0px 20px ${theme.ui}`,
-          }}
-        >
-          Context App
-        </h1>
-        <ul
-          style={{
-            boxShadow: `0px 0px 20px ${theme.ui}`,
-            background: theme.bg,
-          }}
-        >
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      </ThemeContext.Consumer>
     );
   }
 }

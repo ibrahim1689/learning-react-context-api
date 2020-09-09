3639fad28de167c9288c6eb288680ec9719541a2
ibrahim <hello.dev.ibrahim@gmail.com>
2020-09-09 12:42:53 +0530

    used useContext hook in BookList component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  // state = {  }
-  render() {
-    const { isLightTheme, light, dark } = this.context;
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
 
-    const theme = isLightTheme ? light : dark;
+  const theme = isLightTheme ? light : dark;
 
-    return (
-      <div className="book-list">
-        <ul
-          style={{
-            boxShadow: `0px 0px 20px ${theme.ui}`,
-            color: theme.text,
-            background: theme.bg,
-          }}
-        >
-          <li>The Way Of Kings</li>
-          <li>The Name Of The Wind</li>
-          <li>The Final Empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="book-list">
+      <ul
+        style={{
+          boxShadow: `0px 0px 20px ${theme.ui}`,
+          color: theme.text,
+          background: theme.bg,
+        }}
+      >
+        <li>The Way Of Kings</li>
+        <li>The Name Of The Wind</li>
+        <li>The Final Empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
